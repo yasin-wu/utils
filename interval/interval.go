dc2b405319e584f233425d85b977e64767b87bf9
yasin <yasin_wu@qq.com>
2021-06-23 13:51:29 +0800

    add:type EQType

--- interval/interval.go
+++ interval/interval.go
@@ -2,6 +2,15 @@ package interval
 
 import "math"
 
+type EQType string
+
+const (
+	EQType1 EQType = "[]"
+	EQType2 EQType = "(]"
+	EQType3 EQType = "[)"
+	EQType4 EQType = "()"
+)
+
 type Interval struct {
 	Start int64 `json:"start"`
 	End   int64 `json:"end"`
@@ -25,15 +34,15 @@ func (this *Interval) IntervalMixed(interval *Interval) bool {
  * @date: 2021/4/22 13:14
  * @description：i 是否属于interval,取等
  */
-func (this *Interval) Belong(i int64, eq string) bool {
+func (this *Interval) Belong(i int64, eq EQType) bool {
 	switch eq {
-	case "[]":
+	case EQType1:
 		return i >= this.Start && i <= this.End
-	case "(]":
+	case EQType2:
 		return i > this.Start && i <= this.End
-	case "[)":
+	case EQType3:
 		return i >= this.Start && i < this.End
-	case "()":
+	case EQType4:
 		return i > this.Start && i < this.End
 	default:
 		return i >= this.Start && i <= this.End
@@ -46,15 +55,15 @@ func (this *Interval) Belong(i int64, eq string) bool {
  * @date: 2021/4/22 14:58
  * @description：interval是否this的子区间
  */
-func (this *Interval) Contain(interval *Interval, eq string) bool {
+func (this *Interval) Contain(interval *Interval, eq EQType) bool {
 	switch eq {
-	case "[]":
+	case EQType1:
 		return interval.Start >= this.Start && interval.End <= this.End
-	case "(]":
+	case EQType2:
 		return interval.Start > this.Start && interval.End <= this.End
-	case "[)":
+	case EQType3:
 		return interval.Start >= this.Start && interval.End < this.End
-	case "()":
+	case EQType4:
 		return interval.Start > this.Start && interval.End < this.End
 	default:
 		return interval.Start >= this.Start && interval.End <= this.End
