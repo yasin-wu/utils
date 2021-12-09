4ee4ed3bab07476035adb70412f4acf9fa861175
yasin <yasin_wu@qq.com>
2021-12-09 10:27:42 +0800

    del:删除多余函数

--- enum/gender.go
+++ enum/gender.go
@@ -9,11 +9,3 @@ const (
 	GenderMale   Gender = 1 //男
 	GenderFemale Gender = 2 //女
 )
-
-func (c Gender) Int() int {
-	return int(c)
-}
-
-func (c Gender) Equals(i int) bool {
-	return c.Int() == i
-}
