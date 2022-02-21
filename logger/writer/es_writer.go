9552a89bbf9c77c92f2cd9bce3f5c0662a9311a2
yasin <wh_wuyang@topsec.com.cn>
2022-02-21 15:47:16 +0800

    refactor: 删除ESWriter中ID设置

--- logger/writer/es_writer.go
+++ logger/writer/es_writer.go
@@ -5,31 +5,22 @@ import (
 	"errors"
 	"io"
 
-	"github.com/rs/xid"
-
 	"github.com/elastic/go-elasticsearch/v7"
 )
 
 type ESWriter struct {
 	client *elasticsearch.Client
 	index  string
-	idFunc IdFunc
 }
 
 var _ io.Writer = (*ESWriter)(nil)
 
 type ESConfig elasticsearch.Config
 
-type IdFunc func() string
-
-func NewESWriter(index string, config *ESConfig, idFunc ...IdFunc) (*ESWriter, error) {
+func NewESWriter(index string, config *ESConfig) (*ESWriter, error) {
 	esWriter := &ESWriter{index: index}
-	esWriter.idFunc = esWriter.IdFunc
 	if config == nil {
 		return nil, errors.New("elasticsearch config is nil")
-	}
-	if idFunc != nil {
-		esWriter.idFunc = idFunc[0]
 	}
 	client, err := elasticsearch.NewClient(elasticsearch.Config(*config))
 	if err != nil {
@@ -41,7 +32,7 @@ func NewESWriter(index string, config *ESConfig, idFunc ...IdFunc) (*ESWriter, e
 }
 
 func (w *ESWriter) Write(message []byte) (int, error) {
-	resp, err := w.client.Create(w.index, w.idFunc(), bytes.NewReader(message))
+	resp, err := w.client.Index(w.index, bytes.NewReader(message))
 	defer resp.Body.Close()
 	if err != nil {
 		return -1, err
@@ -50,8 +41,4 @@ func (w *ESWriter) Write(message []byte) (int, error) {
 		return -1, errors.New(resp.String())
 	}
 	return 1, nil
-}
-
-func (w *ESWriter) IdFunc() string {
-	return xid.New().String()
 }
