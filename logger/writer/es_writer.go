0b959458999225c95145afe09015d1dfc4fad4d6
yasin <wh_wuyang@topsec.com.cn>
2022-03-02 10:06:16 +0800

    refactor: 更新ESWriter中索引按天存储

--- logger/writer/es_writer.go
+++ logger/writer/es_writer.go
@@ -3,22 +3,24 @@ package writer
 import (
 	"bytes"
 	"errors"
+	"fmt"
 	"io"
+	"time"
 
 	"github.com/elastic/go-elasticsearch/v7"
 )
 
 type ESWriter struct {
-	client *elasticsearch.Client
-	index  string
+	client      *elasticsearch.Client
+	indexPrefix string
 }
 
 var _ io.Writer = (*ESWriter)(nil)
 
 type ESConfig elasticsearch.Config
 
-func NewESWriter(index string, config *ESConfig) (*ESWriter, error) {
-	esWriter := &ESWriter{index: index}
+func NewESWriter(indexPrefix string, config *ESConfig) (*ESWriter, error) {
+	esWriter := &ESWriter{indexPrefix: indexPrefix}
 	if config == nil {
 		return nil, errors.New("elasticsearch config is nil")
 	}
@@ -32,7 +34,7 @@ func NewESWriter(index string, config *ESConfig) (*ESWriter, error) {
 }
 
 func (w *ESWriter) Write(message []byte) (int, error) {
-	resp, err := w.client.Index(w.index, bytes.NewReader(message))
+	resp, err := w.client.Index(w.handleIndex(), bytes.NewReader(message))
 	defer resp.Body.Close()
 	if err != nil {
 		return -1, err
@@ -41,4 +43,12 @@ func (w *ESWriter) Write(message []byte) (int, error) {
 		return -1, errors.New(resp.String())
 	}
 	return 1, nil
+}
+
+func (w *ESWriter) handleIndex() string {
+	now := time.Now()
+	year := now.Format("2006")
+	month := now.Format("01")
+	day := now.Format("02")
+	return fmt.Sprintf("%s_%s_%s_%s", w.indexPrefix, year, month, day)
 }
