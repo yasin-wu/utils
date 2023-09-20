ac7f82d8b033df9b9dba253df8b04ea0b66f44c7
yasin <yasin_wu@qq.com>
2023-09-20 15:17:15 +0800

    feat: func FreePort

--- net/net.go
+++ net/net.go
@@ -3,6 +3,7 @@ package net
 import (
 	"math/big"
 	"net"
+	"strconv"
 )
 
 func InetAtoN(ip string) int64 {
@@ -25,6 +26,21 @@ func ParseCIDR(cidr string) ([]string, error) {
 		return nil, nil
 	}
 	return ips[1 : len(ips)-1], nil
+}
+
+func FreePort() (string, error) {
+	addr, err := net.ResolveTCPAddr("tcp", "localhost:0")
+	if err != nil {
+		return "", err
+	}
+	listener, err := net.ListenTCP("tcp", addr)
+	if err != nil {
+		return "", err
+	}
+	defer func(listener *net.TCPListener) {
+		_ = listener.Close()
+	}(listener)
+	return strconv.Itoa(listener.Addr().(*net.TCPAddr).Port), nil
 }
 
 func inc(ip net.IP) {
